c50b49fe1ac4ee7e0676ace9680928c529e9960c
SOHAM SONAR <soham.2004@outlook.com>
2025-10-21 11:09:23 +0530

    Update program5.c

--- Assignment1/program5.c
+++ Assignment1/program5.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
 
+////////////////////////////////////////////////////////////////////////////////
+//
+//  function name : Accept
+//  description :   It is used to Accept a bumber from user and display that number of * on screen
+//  Input :         integer
+//  Output :        string
+//  Author :        soham sachin sonar
+//  Date :          21/10/2025
+//
+///////////////////////////////////////////////////////////////////////////////
 void Accept(int iNo)
 {
     int iCnt = 0;
 
-    for(iCnt = 1;iCnt <= iNo;iCnt++)
+    for(iCnt = 1;iCnt <= iNo;iCnt++)            // Business logic
     {
         printf("*");
     }
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+///////////////////////////////////////////////////////////////////////////////
 int main()
 {
     int iValue = 0;
